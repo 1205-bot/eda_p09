bcc3e196794c63d52b7379b476ab9bb3a06d3899
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-04-23 16:11:16 +0000

    Subo tupla2.py

---
+++ tupla2.py
@@ -0,0 +1,7 @@
+diasdeMes = (("Enero",31),("Febrero",29),("Marzo",31),("Abril",30),("Mayo",31),("Junio",30),
+            ("Julio",31),("Agosto",31),("Septiembre",30),("Octubre",31),("Noviembre",30),("Diciembre",31))
+
+print(diasdeMes)
+print("{} tiene {} dias".format(diasdeMes[0][0], diasdeMes[0][1]))
+print("{} tiene {} dias".format(diasdeMes[6][0], diasdeMes[6][1]))
+print("{} tiene {} dias".format(diasdeMes[7][0], diasdeMes[7][1]))
